9bd0e2ce8945b949b9a973ba98e46840aa832bfb
kawksung <alstjd5479@naver.com>
2023-01-25 10:00:47 +0900

    start: 구글-포맷 적용

--- inflearn/Start/src/main/java/com/example/start/controller/HelloController.java
+++ inflearn/Start/src/main/java/com/example/start/controller/HelloController.java
@@ -8,42 +8,45 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
-    @GetMapping("hello")
-    public String hello(Model model){
-        model.addAttribute("data","hello!!");//key = "data" value="hello!!"
-        return "hello";
+
+  @GetMapping("hello")
+  public String hello(Model model) {
+    model.addAttribute("data", "hello!!");//key = "data" value="hello!!"
+    return "hello";
+  }
+
+  @GetMapping("hello-mvc")
+  public String helloMvc(@RequestParam(name = "name") String name,
+      Model model) { //@RequestParam() 주소 뒤에 ? 값으로 들어오는 속성
+    model.addAttribute("name", name);
+    return "hello-template";
+  }
+
+  @GetMapping("hello-string")
+  @ResponseBody
+  public String helloString(@RequestParam("name") String name) {
+    return "hello " + name; // view에 매핑 x
+  }
+
+  @GetMapping("hello-api")
+  @ResponseBody
+  public Hello helloApi(@RequestParam("name") String name) {
+    Hello hello = new Hello(); //객체를 넘기면 default로 Json으로 보낸다
+    hello.setName(name);
+    return hello;
+  }
+
+  static class Hello {
+
+    private String name;
+
+    public String getName() {
+      return name;
     }
 
-    @GetMapping("hello-mvc")
-    public String helloMvc(@RequestParam(name = "name") String name, Model model){ //@RequestParam() 주소 뒤에 ? 값으로 들어오는 속성
-        model.addAttribute("name",name);
-        return "hello-template";
+    public void setName(String name) {
+      this.name = name;
     }
-
-    @GetMapping("hello-string")
-    @ResponseBody
-    public String helloString(@RequestParam("name") String name){
-        return "hello "+name; // view에 매핑 x
-    }
-
-    @GetMapping("hello-api")
-    @ResponseBody
-    public Hello helloApi(@RequestParam("name") String name){
-        Hello hello=new Hello(); //객체를 넘기면 default로 Json으로 보낸다
-        hello.setName(name);
-        return hello;
-    }
-
-    static class Hello{
-        private String name;
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-    }
+  }
 
 }
