7046d37b5080ef58a0b36afdc775bb739316d837
kawksung <alstjd5479@naver.com>
2023-01-29 16:07:02 +0900

    start: H2로 순수 JDBC 데이터 베이스 사용

--- inflearn/Start/src/main/java/com/example/start/SpringConfig.java
+++ inflearn/Start/src/main/java/com/example/start/SpringConfig.java
@@ -3,19 +3,30 @@ package com.example.start;
 import com.example.start.repository.MemberRepository;
 import com.example.start.repository.MemoryMemberRepository;
 import com.example.start.service.MemberService;
+import hello.hellospring.repository.JdbcMemberRepository;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
 
+  private DataSource dataSource;
+
+  @Autowired
+  public SpringConfig(DataSource dataSource) {
+    this.dataSource = dataSource;
+  }
+
   @Bean
-  public MemberService memberService(){
+  public MemberService memberService() {
     return new MemberService(memberRepository());
   }
 
   @Bean
-  public MemberRepository memberRepository(){
-    return new MemoryMemberRepository();
+  public MemberRepository memberRepository() {
+    // return new MemoryMemberRepository();
+    return new JdbcMemberRepository(dataSource);
   }
 }
