6034a3392bda94920dfa235a8edb8d8f32966f68
kawksung <alstjd5479@naver.com>
2023-02-02 22:37:17 +0900

    start: AOP 사용

--- inflearn/Start/src/main/java/com/example/start/SpringConfig.java
+++ inflearn/Start/src/main/java/com/example/start/SpringConfig.java
@@ -1,5 +1,6 @@
 package com.example.start;
 
+import com.example.start.Aop.TimeTraceAop;
 import com.example.start.repository.JdbcTemplateMemberRepository;
 import com.example.start.repository.JpaMemberRepository;
 import com.example.start.repository.MemberRepository;
@@ -26,6 +27,11 @@ public class SpringConfig {
   public MemberService memberService() {
     return new MemberService(memberRepository);
   }
+
+//  @Bean
+//  public TimeTraceAop timeTraceAop() {
+//    return new TimeTraceAop();
+//  }
 
 //  @Bean
 //  public MemberRepository memberRepository() {
