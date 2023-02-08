66073e024107bd790959d749586557c6068f5ea6
kawksung <alstjd5479@naver.com>
2023-02-08 20:27:44 +0900

    Basic: Appconfig 유지 보수 쉽게 리팩터링

--- inflearn/Basic/src/main/java/hello/basic/AppConfig.java
+++ inflearn/Basic/src/main/java/hello/basic/AppConfig.java
@@ -1,5 +1,6 @@
 package hello.basic;
 
+import hello.basic.discount.DiscountPolicy;
 import hello.basic.discount.FixDiscountPolicy;
 import hello.basic.member.MemberService;
 import hello.basic.member.MemberServiceImpl;
@@ -10,10 +11,18 @@ import hello.basic.order.OrderServiceImpl;
 public class AppConfig {
 
   public MemberService memberService() {
-    return new MemberServiceImpl(new MemoryMemberRepository());
+    return new MemberServiceImpl(MemberRepository());
+  }
+
+  private MemoryMemberRepository MemberRepository() {
+    return new MemoryMemberRepository();
   }
 
   public OrderService orderService() {
-    return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    return new OrderServiceImpl(MemberRepository(), discountPolicy());
+  }
+
+  public DiscountPolicy discountPolicy() {
+    return new FixDiscountPolicy();
   }
 }
