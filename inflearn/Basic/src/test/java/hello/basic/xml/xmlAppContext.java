ce7f5704d69e06a874a6b64057f804567bf2ba6e
kawksung <alstjd5479@naver.com>
2023-02-09 21:09:48 +0900

    Basic: 레거시 xml로 Bean 등록

---
+++ inflearn/Basic/src/test/java/hello/basic/xml/xmlAppContext.java
@@ -0,0 +1,18 @@
+package hello.basic.xml;
+
+import hello.basic.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class xmlAppContext {
+
+  @Test
+  void xmlAppContext() {
+    GenericXmlApplicationContext ac = new GenericXmlApplicationContext(
+        "appConfig.xml");
+    MemberService memberService = ac.getBean("memberService", MemberService.class);
+    Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+  }
+
+}
