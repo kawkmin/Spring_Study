f9a6620d549f91e802751f23cb100180a03eb6da
kawksung <alstjd5479@naver.com>
2023-02-18 22:23:59 +0900

    Basic: @ComponentScan의 탐색 위치와 스캔 대상

--- inflearn/Basic/src/main/java/hello/basic/AutoAppConfig.java
+++ inflearn/Basic/src/main/java/hello/basic/AutoAppConfig.java
@@ -6,8 +6,11 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+    basePackages = "hello.basic.member", //default = @ComponentScan의 패키지 위치
+    basePackageClasses = AutoAppConfig.class, // default = @ComponentScan의 클래스
     excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
     // APPConfig의 @configuration은 @component가 있으니, 제외 해주자
+    // 그 외에도 @Controller @Sevice 등 더 있다.
 )
 public class AutoAppConfig {
 
