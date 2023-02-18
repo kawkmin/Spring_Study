972a49be1a57a15878c29135bebae120c0094956
kawksung <alstjd5479@naver.com>
2023-02-18 22:35:42 +0900

    Basic: Bean의 이름이 중복이 될 때,

--- inflearn/Basic/src/main/java/hello/basic/AutoAppConfig.java
+++ inflearn/Basic/src/main/java/hello/basic/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.basic;
 
+import hello.basic.member.MemberRepository;
+import hello.basic.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -14,4 +17,10 @@ import org.springframework.context.annotation.FilterType;
 )
 public class AutoAppConfig {
 
+  @Bean
+    // @Bean(name = "memoryMemberRepository")
+    // 자동으로 생성되는 memoryMemberRepository Bean의 이름이 같지만, 수동이 우선권을 가짐
+  MemberRepository memberRepository() {
+    return new MemoryMemberRepository();
+  }
 }
