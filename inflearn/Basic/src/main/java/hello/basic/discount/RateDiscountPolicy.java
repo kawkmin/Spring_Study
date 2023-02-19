d2c51d0e0b07912240e164c63cbed6c72ba42fb9
kawksung <alstjd5479@naver.com>
2023-02-19 22:47:37 +0900

    Basic: 생성할 때, 매칭할 빈이 여러개 일 때, 오류 -> 해결법 3가지

--- inflearn/Basic/src/main/java/hello/basic/discount/RateDiscountPolicy.java
+++ inflearn/Basic/src/main/java/hello/basic/discount/RateDiscountPolicy.java
@@ -2,9 +2,13 @@ package hello.basic.discount;
 
 import hello.basic.member.Grade;
 import hello.basic.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+//@Qualifier("mainDiscountPolicy") // 여러개의 빈일 때, 이름으로 매칭하게 (@Primary 보다 우선순위 높음)
+@Primary // 여러개의 빈일 때, 우선으로
 public class RateDiscountPolicy implements DiscountPolicy {
 
   private int discountPercent = 10;
--- inflearn/Basic/src/main/java/hello/basic/order/OrderServiceImpl.java
+++ inflearn/Basic/src/main/java/hello/basic/order/OrderServiceImpl.java
@@ -5,15 +5,16 @@ import hello.basic.member.Member;
 import hello.basic.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor // 생성자 자동
+//@RequiredArgsConstructor // 생성자 자동
 public class OrderServiceImpl implements OrderService {
 
   // @Autowired 필드 주입 (테스트가 힘들어 비권장)
   private final MemberRepository memberRepository;
-  private final DiscountPolicy discountPolicy;
+  private final DiscountPolicy discountPolicy; //rateDiscountPolicy = 빈이 2개일 때, 파라미터로 매칭하게 가능
 
   /* @Atuowired setter 주입
   public void setMemberRepository(MemberRepository memberRepository) {
@@ -21,10 +22,11 @@ public class OrderServiceImpl implements OrderService {
   } */
 
   // @Autowired 생성자가 한개면, 생략 가능
-/*  public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+  public OrderServiceImpl(MemberRepository memberRepository,
+      /*@Qualifier("mainDiscountPolicy") 이름으로 매칭하게*/ DiscountPolicy discountPolicy) {
     this.memberRepository = memberRepository;
     this.discountPolicy = discountPolicy;
-  }*/
+  }
 
   /* 일반 메서드 주입(잘 사용하지 않음)
   @Autowired
