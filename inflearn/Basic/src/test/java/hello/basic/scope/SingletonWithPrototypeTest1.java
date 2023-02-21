98cd65138763cc106205d775c1d900aca46312c1
kawksung <alstjd5479@naver.com>
2023-02-21 23:42:02 +0900

    Basic: 싱글톤 빈과 함께 사용하여, 매번 새로 안만들어지는 프로토타입 빈을 provider로 해결 (자바 표준)

--- inflearn/Basic/src/test/java/hello/basic/scope/SingletonWithPrototypeTest1.java
+++ inflearn/Basic/src/test/java/hello/basic/scope/SingletonWithPrototypeTest1.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import jakarta.inject.Provider;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,10 +44,10 @@ public class SingletonWithPrototypeTest1 {
   static class ClientBean {
 
     @Autowired
-    private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+    private Provider<PrototypeBean> prototypeBeanProvider;
 
     public int logic() {
-      PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+      PrototypeBean prototypeBean = prototypeBeanProvider.get();
       prototypeBean.addCount();
       int count = prototypeBean.getCount();
       return count;
