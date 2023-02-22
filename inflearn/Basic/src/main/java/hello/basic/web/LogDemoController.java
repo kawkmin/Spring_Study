44cd7dca15c0ba0e83f97d513b0d7872e7619c50
kawksung <alstjd5479@naver.com>
2023-02-22 23:43:37 +0900

    Basic: proxyMode로 가짜로 클래스를 만들어, 빈의 대리인 역할을 한다

--- inflearn/Basic/src/main/java/hello/basic/web/LogDemoController.java
+++ inflearn/Basic/src/main/java/hello/basic/web/LogDemoController.java
@@ -14,13 +14,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LogDemoController {
 
   private final LogDemoService logDemoService;
-  private final ObjectProvider<MyLogger> myLoggerProvider;
+  private final MyLogger myLogger;
 
   @RequestMapping("log-demo")
   @ResponseBody
   public String logDemo(HttpServletRequest request) throws InterruptedException {
     String requestURL = request.getRequestURI().toString();
-    MyLogger myLogger = myLoggerProvider.getObject(); //스코프가 최초로 만들어지는 곳
+    //MyLogger myLogger = myLoggerProvider.getObject(); //스코프가 최초로 만들어지는 곳
+
+    System.out.println("myLogger = " + myLogger.getClass());
     myLogger.setRequestURL(requestURL);
 
     myLogger.log("controller test");
