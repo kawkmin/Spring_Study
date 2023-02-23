33cc4a9db5b144042a076d9707b32a378b395c00
kawksung <alstjd5479@naver.com>
2023-02-23 14:20:17 +0900

    Basic: (추가 학습)Bean 생성자 순서 확인

--- inflearn/Basic/src/main/java/hello/basic/AppConfig.java
+++ inflearn/Basic/src/main/java/hello/basic/AppConfig.java
@@ -15,25 +15,27 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
 
   @Bean
-  public MemoryMemberRepository memberRepository() {
-    System.out.println("call AppConfig.memberRepository");
-    return new MemoryMemberRepository();
-  }
-
-  @Bean
-  public MemberService memberService() {
-    System.out.println("call AppConfig.memberService");
-    return new MemberServiceImpl(memberRepository());
-  }
-
-  @Bean
   public OrderService orderService() {
-    System.out.println("call AppConfig.orderService");
+    System.out.println("call AppConfig.orderService");// 1
     return new OrderServiceImpl(memberRepository(), discountPolicy());
   }
 
   @Bean
+  public MemberService memberService() {
+    System.out.println("call AppConfig.memberService"); // 4
+    return new MemberServiceImpl(memberRepository());
+  }
+
+  @Bean
+  public MemoryMemberRepository memberRepository() {
+    System.out.println("call AppConfig.memberRepository"); // 2
+    return new MemoryMemberRepository();
+  }
+
+
+  @Bean
   public DiscountPolicy discountPolicy() {
+    System.out.println("call AppConfig.discountPolicy"); // 3
 //    return new FixDiscountPolicy();
     return new RateDiscountPolicy();
   }
