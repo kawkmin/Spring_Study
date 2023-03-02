34f424d2cfab7598036073b9ab9408732679735e
kawksung <alstjd5479@naver.com>
2023-03-02 15:41:20 +0900

    MVC1: 핸들러 매핑,어뎁터, 뷰 리졸버 레거시 코드

--- inflearn/MVC1/src/main/java/hello/servlet/Mvc1Application.java
+++ inflearn/MVC1/src/main/java/hello/servlet/Mvc1Application.java
@@ -3,6 +3,8 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @ServletComponentScan //서블릿 자동 등록
 @SpringBootApplication
@@ -11,5 +13,13 @@ public class Mvc1Application {
   public static void main(String[] args) {
     SpringApplication.run(Mvc1Application.class, args);
   }
+
+/*
+  ModelAndView 에서 해줌
+  @Bean
+  InternalResourceViewResolver internalResourceViewResolver() {
+    return new InternalResourceViewResolver("/WEB-INF/views/", ".jsp");
+  }
+*/
 
 }
