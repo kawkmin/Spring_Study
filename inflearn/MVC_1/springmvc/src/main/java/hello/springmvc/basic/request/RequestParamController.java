1f42126b67428e6c9cebf219aae4918df7c86d97
kawksung <alstjd5479@naver.com>
2023-03-03 15:21:08 +0900

    MVC1_func: 요청 파라미터 - @ModelAttribute 로 객체를 이용하여 사용

--- inflearn/MVC_1/springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ inflearn/MVC_1/springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -1,11 +1,13 @@
 package hello.springmvc.basic.request;
 
+import hello.springmvc.basic.HelloData;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -77,6 +79,33 @@ public class RequestParamController {
   public String requestParamMap(
       @RequestParam Map<String, Object> paramMap) {
     log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
+    return "ok";
+  }
+
+/*  @ResponseBody
+  @RequestMapping("/model-attribute-v1")
+  public String modelAttributeV1(@RequestParam String username, @RequestParam int age) {
+    HelloData helloData = new HelloData();
+    helloData.setUsername(username);
+    helloData.setAge(age);
+
+    log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+    log.info("helloData={}", helloData); //@Data 는 @ToString 있음
+    return "ok";*/
+
+  @ResponseBody
+  @RequestMapping("/model-attribute-v1")
+  public String modelAttributeV1(@ModelAttribute HelloData helloData) {   // 객체의 프로퍼티를 찾아 파라미터 입력
+    log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+    log.info("helloData={}", helloData); //@Data 는 @ToString 있음
+    return "ok";
+  }
+
+  @ResponseBody
+  @RequestMapping("/model-attribute-v2")
+  public String modelAttributeV2(HelloData helloData) {
+    log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+    log.info("helloData={}", helloData);
     return "ok";
   }
 }
