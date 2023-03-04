01777cee28f13bbc8dbc6d50d405fa2dca4560df
kawksung <alstjd5479@naver.com>
2023-03-04 20:42:21 +0900

    MVC1_item_server: 상품 상세 구현

--- inflearn/MVC_1/item-service/src/main/java/hello/itemservice/domain/web/basic/BasicItemController.java
+++ inflearn/MVC_1/item-service/src/main/java/hello/itemservice/domain/web/basic/BasicItemController.java
@@ -8,12 +8,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/basic/items")
 @RequiredArgsConstructor
-public class BasicController {
+public class BasicItemController {
 
   private final ItemRepository itemRepository;
 
@@ -27,6 +28,13 @@ public class BasicController {
     List<Item> items = itemRepository.findAll();
     model.addAttribute("items", items);
     return "basic/items";
+  }
+
+  @GetMapping("{itemID}")
+  public String item(@PathVariable long itemID, Model model) {
+    Item item = itemRepository.findById(itemID);
+    model.addAttribute("item", item);
+    return "basic/item";
   }
 
   @PostConstruct
