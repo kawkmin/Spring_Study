c601ba0570f78951f0e9b2aa919946cc7af710d0
kawksung <alstjd5479@naver.com>
2023-03-10 00:00:38 +0900

    MVC2: 다중 체크박스일때, 구현

--- inflearn/MVC_2/form/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ inflearn/MVC_2/form/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -2,6 +2,8 @@ package hello.itemservice.web.form;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -19,7 +21,17 @@ public class FormItemController {
 
   private final ItemRepository itemRepository;
 
+  @ModelAttribute("regions") //자동으로 모델에 regions 이름으로 return 값이 벨류로 들어감
+  public Map<String, String> regions() {
+    Map<String, String> regions = new LinkedHashMap<>();
+    regions.put("SEOUL", "서울");
+    regions.put("BUSAN", "부산");
+    regions.put("JEJU", "제주");
+    return regions;
+  }
+
   @GetMapping
+
   public String items(Model model) {
     List<Item> items = itemRepository.findAll();
     model.addAttribute("items", items);
@@ -30,18 +42,33 @@ public class FormItemController {
   public String item(@PathVariable long itemId, Model model) {
     Item item = itemRepository.findById(itemId);
     model.addAttribute("item", item);
+
+    Map<String, String> regions = new LinkedHashMap<>();
+    regions.put("SEOUL", "서울");
+    regions.put("BUSAN", "부산");
+    regions.put("JEJU", "제주");
+    model.addAttribute("regions", regions);
+
     return "form/item";
   }
 
   @GetMapping("/add")
   public String addForm(Model model) {
     model.addAttribute("item", new Item());
+
+    Map<String, String> regions = new LinkedHashMap<>();
+    regions.put("SEOUL", "서울");
+    regions.put("BUSAN", "부산");
+    regions.put("JEJU", "제주");
+    model.addAttribute("regions", regions);
+
     return "form/addForm";
   }
 
   @PostMapping("/add")
   public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
     log.info("item.open={}", item.getOpen());
+    log.info("item.regions={}", item.getRegions());
 
     Item savedItem = itemRepository.save(item);
     redirectAttributes.addAttribute("itemId", savedItem.getId());
@@ -52,6 +79,13 @@ public class FormItemController {
   @GetMapping("/{itemId}/edit")
   public String editForm(@PathVariable Long itemId, Model model) {
     Item item = itemRepository.findById(itemId);
+
+    Map<String, String> regions = new LinkedHashMap<>();
+    regions.put("SEOUL", "서울");
+    regions.put("BUSAN", "부산");
+    regions.put("JEJU", "제주");
+
+    model.addAttribute("regions", regions);
     model.addAttribute("item", item);
     return "form/editForm";
   }
