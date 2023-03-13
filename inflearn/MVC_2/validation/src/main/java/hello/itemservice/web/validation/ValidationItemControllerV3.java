0b9d09a2f50c71279340394afcde9d96c344d3d6
kawksung <alstjd5479@naver.com>
2023-03-13 12:02:27 +0900

    MVC2: v3 - 오브젝트 오류 처리

--- inflearn/MVC_2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
+++ inflearn/MVC_2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
@@ -48,6 +48,13 @@ public class ValidationItemControllerV3 {
   public String addItem(@Validated @ModelAttribute Item item, BindingResult bindingResult,
       RedirectAttributes redirectAttributes, Model model) {
 
+    if (item.getPrice() != null && item.getQuantity() != null) {
+      int resultPrice = item.getPrice() * item.getQuantity();
+      if (resultPrice < 10000) {
+        bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+      }
+    }
+
     // 에러 로직
     if (bindingResult.hasErrors()) {
       log.info("errors={}", bindingResult);
