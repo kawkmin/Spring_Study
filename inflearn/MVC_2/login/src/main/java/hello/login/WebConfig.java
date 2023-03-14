bf1bd03d8cecd9f219da54f8a7ea9cee90e03040
kawksung <alstjd5479@naver.com>
2023-03-15 00:15:44 +0900

    MVC2: 인터셉터로 로그인 체크 로직 구현

--- inflearn/MVC_2/login/src/main/java/hello/login/WebConfig.java
+++ inflearn/MVC_2/login/src/main/java/hello/login/WebConfig.java
@@ -3,6 +3,7 @@ package hello.login;
 import hello.login.web.filter.Logfilter;
 import hello.login.web.filter.LoginCheckFilter;
 import hello.login.web.interceptor.LogInterceptor;
+import hello.login.web.interceptor.LoginCheckInterceptor;
 import javax.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -19,6 +20,12 @@ public class WebConfig implements WebMvcConfigurer {
         .order(1)
         .addPathPatterns("/**")
         .excludePathPatterns("/css/**", "/*.ico", "/error");
+
+    registry.addInterceptor(new LoginCheckInterceptor())
+        .order(2)
+        .addPathPatterns("/**")
+        .excludePathPatterns("/", "/members/add", "/login", "logout", "/css/**", "/*.ico",
+            "/error");
   }
 
   //@Bean
