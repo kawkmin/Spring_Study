e4451389dbd9ab201556e2c338784dfa3a6683d8
kawksung <alstjd5479@naver.com>
2023-03-17 17:08:22 +0900

    DB1: 데이터 베이스 등록,조회 구현

---
+++ inflearn/DB1/jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -0,0 +1,99 @@
+package hello.jdbc.repository;
+
+import hello.jdbc.collection.DBConnectionUtil;
+import hello.jdbc.domain.Member;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.NoSuchElementException;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * jdbc- DriverManger 사용
+ */
+@Slf4j
+public class MemberRepositoryV0 {
+
+  public Member save(Member member) throws SQLException {
+    String sql = "insert into member(member_id, money) values (?,?)";
+
+    Connection con = null;
+    PreparedStatement pstmt = null;
+
+    con = getConnection();
+    try {
+      pstmt = con.prepareStatement(sql);
+      pstmt.setString(1, member.getMemberId());
+      pstmt.setInt(2, member.getMoney());
+      pstmt.executeUpdate();
+      return member;
+    } catch (SQLException e) {
+      log.error("db error", e);
+      throw e;
+    } finally {
+      close(con, pstmt, null);
+    }
+  }
+
+  public Member findById(String memberId) throws SQLException {
+    String sql = "select * from member where member_id =?";
+
+    Connection con = null;
+    PreparedStatement pstmt = null;
+    ResultSet rs = null;
+
+    try {
+      con = getConnection();
+      pstmt = con.prepareStatement(sql);
+      pstmt.setString(1, memberId);
+
+      rs = pstmt.executeQuery();
+      if (rs.next()) {
+        Member member = new Member();
+        member.setMemberId(rs.getString("member_id"));
+        member.setMoney(rs.getInt("money"));
+        return member;
+      } else {
+        throw new NoSuchElementException("member no found memberId=" + memberId);
+      }
+    } catch (SQLException e) {
+      log.error("db error", e);
+      throw e;
+    } finally {
+      close(con, pstmt, rs);
+    }
+  }
+
+  private void close(Connection con, Statement stmt, ResultSet rs) {
+
+    if (rs != null) {
+      try {
+        rs.close();
+      } catch (SQLException e) {
+        log.info("error", e);
+      }
+    }
+
+    if (stmt != null) {
+      try {
+        stmt.close();
+      } catch (SQLException e) {
+        log.info("error", e);
+      }
+    }
+
+    if (con != null) {
+      try {
+        con.close();
+      } catch (SQLException e) {
+        log.info("error", e);
+      }
+    }
+  }
+
+  private static Connection getConnection() {
+    return DBConnectionUtil.getConnection();
+  }
+}
