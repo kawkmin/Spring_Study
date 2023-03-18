e32416b722394faf1fb62f1c0f690eaf7bee1737
kawksung <alstjd5479@naver.com>
2023-03-18 16:51:24 +0900

    DB1: Spring 제공 없이, 직접 트랜잭션 적용

---
+++ inflearn/DB1/jdbc/src/main/java/hello/jdbc/service/MemberServiceV2.java
@@ -0,0 +1,64 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV2;
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 트랜잭션 - 파라미터 연동, 풀을 고려한 종료
+ */
+@Slf4j
+@RequiredArgsConstructor
+public class MemberServiceV2 {
+
+  private final DataSource dataSource;
+  private final MemberRepositoryV2 memberRepository;
+
+  public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+
+    Connection con = dataSource.getConnection();
+    try {
+      con.setAutoCommit(false);
+
+      bizLogic(con, fromId, toId, money);
+      con.commit();
+    } catch (Exception e) {
+      con.rollback(); //실패시 롤백
+      throw new IllegalStateException(e);
+    } finally {
+      release(con);
+    }
+
+  }
+
+  private void bizLogic(Connection con, String fromId, String toId, int money)
+      throws SQLException {
+    Member fromMember = memberRepository.findById(con, fromId);
+    Member toMember = memberRepository.findById(con, toId);
+
+    memberRepository.update(con, fromId, fromMember.getMoney() - money);
+    validation(toMember);
+    memberRepository.update(con, toId, toMember.getMoney() + money);
+  }
+
+  private static void release(Connection con) {
+    if (con != null) {
+      try {
+        con.setAutoCommit(true); // 기본값으로 돌려줘야함
+        con.close();
+      } catch (Exception e) {
+        log.info("error", e);
+      }
+    }
+  }
+
+  private static void validation(Member toMember) {
+    if (toMember.getMemberId().equals("ex")) {
+      throw new IllegalStateException("이체중 예외 발생");
+    }
+  }
+}
