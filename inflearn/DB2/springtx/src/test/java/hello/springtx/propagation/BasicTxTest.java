62b7985af0937d1bac55d71b22dbed175ba4b30a
kawksung <alstjd5479@naver.com>
2023-03-26 19:59:18 +0900

    db2: 트랜잭션 내부 외부 사용

--- inflearn/DB2/springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ inflearn/DB2/springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -11,6 +11,7 @@ import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 @Slf4j
 @SpringBootTest
@@ -49,6 +50,22 @@ public class BasicTxTest {
     TransactionStatus tx2 = txManager.getTransaction(new DefaultTransactionAttribute());
     log.info("트랜잭션2 커밋 시작");
     txManager.commit(tx2);
+  }
+
+  @Test
+  void inner_commit() {
+    log.info("외부 트랜잭션 시작");
+    TransactionStatus outer = txManager.getTransaction(new DefaultTransactionDefinition());
+    log.info("outer.isNewTransaction()={}", outer.isNewTransaction());
+
+    log.info("내부 트랜잭션 시작");
+    TransactionStatus inner = txManager.getTransaction(new DefaultTransactionDefinition());
+    log.info("inner.isNewTransaction()={}", inner.isNewTransaction());
+    log.info("내부 트랜잭션 커밋");
+    txManager.commit(inner);
+
+    log.info("외부 트랜잭션 커밋");
+    txManager.commit(outer);
   }
 }
 
