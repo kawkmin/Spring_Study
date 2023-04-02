36b3049a1c8cdff9ae99bb89daca95665384c8fa
kawksung <alstjd5479@naver.com>
2023-04-02 22:08:18 +0900

    JPA_0: 다대다 (사용 X)

--- inflearn/JPA_0/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ inflearn/JPA_0/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
@@ -28,6 +30,10 @@ public class Member {
   @OneToOne
   @JoinColumn(name = "LOCKER_ID")
   private Locker locker;
+
+  @ManyToMany
+  @JoinTable(name = "MEMBER_PRODUCT")
+  private List<Product> products = new ArrayList<>();
 
   @OneToMany
   private List<Member> members = new ArrayList<>();
