acc7508ca823612470fd59f1f320089b078162be
kawksung <alstjd5479@naver.com>
2023-04-04 19:50:16 +0900

    JPA_0: 값 타입의 비교 eqauls

--- inflearn/JPA_0/ex1-hello-jpa/src/main/java/hellojpa/Address.java
+++ inflearn/JPA_0/ex1-hello-jpa/src/main/java/hellojpa/Address.java
@@ -1,5 +1,6 @@
 package hellojpa;
 
+import java.util.Objects;
 import javax.persistence.Embeddable;
 import javax.persistence.Embedded;
 
@@ -41,5 +42,23 @@ public class Address {
 
   private void setZipcode(String zipcode) {
     this.zipcode = zipcode;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Address address = (Address) o;
+    return Objects.equals(city, address.city) && Objects.equals(street,
+        address.street) && Objects.equals(zipcode, address.zipcode);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(city, street, zipcode);
   }
 }
--- inflearn/JPA_0/ex1-hello-jpa/src/main/java/hellojpa/ValueMain.java
+++ inflearn/JPA_0/ex1-hello-jpa/src/main/java/hellojpa/ValueMain.java
@@ -3,11 +3,16 @@ package hellojpa;
 public class ValueMain {
 
   public static void main(String[] args) {
-    Integer a = new Integer(10);
-    Integer b = a;
 
-    a=20;
-    System.out.println("a = " + a);
-    System.out.println("b = " + b);
+    int a = 10;
+    int b = 10;
+    System.out.println(a == b);
+
+    Address address1 = new Address("city", "street", "1000");
+    Address address2 = new Address("city", "street", "1000");
+
+    System.out.println(address1 == address2);
+    System.out.println(address1.equals(address2));
+
   }
 }
