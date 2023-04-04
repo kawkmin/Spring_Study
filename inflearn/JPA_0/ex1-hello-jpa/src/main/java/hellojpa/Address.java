3d76af03f1960ab88857e714def944b092c3232c
kawksung <alstjd5479@naver.com>
2023-04-04 17:20:01 +0900

    JPA_0: 값 타입과 불변

--- inflearn/JPA_0/ex1-hello-jpa/src/main/java/hellojpa/Address.java
+++ inflearn/JPA_0/ex1-hello-jpa/src/main/java/hellojpa/Address.java
@@ -18,4 +18,28 @@ public class Address {
     this.street = street;
     this.zipcode = zipcode;
   }
+
+  public String getCity() {
+    return city;
+  }
+
+  private void setCity(String city) {
+    this.city = city;
+  }
+
+  public String getStreet() {
+    return street;
+  }
+
+  private void setStreet(String street) {
+    this.street = street;
+  }
+
+  public String getZipcode() {
+    return zipcode;
+  }
+
+  private void setZipcode(String zipcode) {
+    this.zipcode = zipcode;
+  }
 }
--- inflearn/JPA_0/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ inflearn/JPA_0/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -15,13 +15,24 @@ public class JpaMain {
     tx.begin();
 
     try {
+      Address address = new Address("city", "street", "1000");
 
       Member member = new Member();
-      member.setName("hello");
-      member.setHomeAddress(new Address("city", "street", "1000"));
-      member.setPeriod(new Period());
-
+      member.setName("member1");
+      member.setHomeAddress(address);
       em.persist(member);
+
+////      Address copyAddress = new Address(address.getCity(), address.getStreet(), address.getZipcode());
+//
+//      Member member2 = new Member();
+//      member2.setName("member2");
+//      member2.setHomeAddress(address);
+//      em.persist(member2);
+//
+////      member.getHomeAddress().setCity("newCity"); // 둘 다 바뀜
+
+      Address newAddress = new Address("newCity", address.getStreet(), address.getZipcode());
+      member.setHomeAddress(newAddress); // 항상 새로 만들어서 변경
 
       tx.commit();
     } catch (Exception e) {
