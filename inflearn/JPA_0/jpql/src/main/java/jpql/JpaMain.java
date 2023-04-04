c9532449a778afd5671fbeda7586c0cfc09b7ff9
kawksung <alstjd5479@naver.com>
2023-04-04 22:41:22 +0900

    JPA_0: JPQL 기본 문법 쿼리

--- inflearn/JPA_0/jpql/src/main/java/jpql/JpaMain.java
+++ inflearn/JPA_0/jpql/src/main/java/jpql/JpaMain.java
@@ -5,6 +5,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 public class JpaMain {
 
@@ -16,6 +18,27 @@ public class JpaMain {
     tx.begin();
 
     try {
+
+      Member member = new Member();
+      member.setUsername("member1");
+      member.setAge(10);
+      em.persist(member);
+
+//      TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
+//
+//      List<Member> resultList = query1.getResultList(); //여러개 일 때 (결과 없으면 빈 리스트)
+//
+//      Member re = query1.getSingleResult(); //한 개 일때 (결과가 한개가 아니면, 오류)
+//
+//      TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class);
+//      Query query3 = em.createQuery("select m.username,m.age from Member m");
+
+      TypedQuery<Member> query = em.createQuery(
+          "select m from Member m where  m.username=:username",
+          Member.class); //:1 (1,"member1) <-위치 기준(비추천)
+      query.setParameter("username", "member1");
+      Member singleResult = query.getSingleResult();
+      System.out.println(singleResult);
 
       tx.commit();
     } catch (Exception e) {
