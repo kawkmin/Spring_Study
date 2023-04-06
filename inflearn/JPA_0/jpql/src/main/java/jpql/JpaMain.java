251d73204a07a84cc23a515574ab8d60fe3f6c1c
kawksung <alstjd5479@naver.com>
2023-04-06 11:11:04 +0900

    JPA_0: 벌크 연산

--- inflearn/JPA_0/jpql/src/main/java/jpql/JpaMain.java
+++ inflearn/JPA_0/jpql/src/main/java/jpql/JpaMain.java
@@ -29,27 +29,30 @@ public class JpaMain {
 
       Member member1 = new Member();
       member1.setUsername("회원1");
+      member1.setAge(0);
       member1.setTeam(teamA);
       em.persist(member1);
+
       Member member2 = new Member();
       member2.setUsername("회원2");
+      member2.setAge(0);
       member2.setTeam(teamA);
       em.persist(member2);
+
       Member member3 = new Member();
       member3.setUsername("회원3");
+      member3.setAge(0);
       member3.setTeam(teamB);
       em.persist(member3);
 
-      em.flush();
-      em.clear();
+      int resultCount = em.createQuery("update Member m set m.age= 20")
+          .executeUpdate();
+      System.out.println("resultCount = " + resultCount);
 
-      List<Member> result = em.createNamedQuery("Member.findByUsername", Member.class)
-          .setParameter("username", "회원1")
-          .getResultList();
+      em.clear(); //없으면 member1 나이를 0으로 가져옴
+      Member findMember1 = em.find(Member.class, member1.getId());
 
-      for (Member member : result) {
-        System.out.println("member = " + member);
-      }
+      System.out.println("member1 = " + findMember1.getAge());
 
       tx.commit();
     } catch (Exception e) {
