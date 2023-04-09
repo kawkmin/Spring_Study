c656f4a4a8871aaa53ecdfd720cd33871693c577
kawksung <alstjd5479@naver.com>
2023-04-09 17:28:30 +0900

    JPA_2: (컬렉션) 주문 조회 구현 - V4: JPA에서 DTO 직접 조회 (N+1 발생)

--- inflearn/JPA_1/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ inflearn/JPA_1/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -9,6 +9,8 @@ import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.query.OrderQueryDto;
+import jpabook.jpashop.repository.order.query.OrderQueryRepository;
 import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -21,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderApiController {
 
   private final OrderRepository orderRepository;
+  private final OrderQueryRepository orderQueryRepository;
 
   @GetMapping("api/v1/orders")
   public List<Order> ordersV1() {
@@ -56,11 +59,16 @@ public class OrderApiController {
   public List<OrderDto> ordersV3_page(
       @RequestParam(value = "offset", defaultValue = "0") int offset,
       @RequestParam(value = "limit", defaultValue = "100") int limit) {
-    List<Order> orders = orderRepository.findAllWithMemberDelivery(offset,limit);
+    List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
 
     return orders.stream()
         .map(o -> new OrderDto(o))
         .collect(Collectors.toList());
+  }
+
+  @GetMapping("api/v4/orders")
+  public List<OrderQueryDto> ordersV4() {
+    return orderQueryRepository.findOrderQueryDtos();
   }
 
   @Getter
