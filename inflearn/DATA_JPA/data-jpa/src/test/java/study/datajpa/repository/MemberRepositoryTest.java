ffdd9640c711a27577ba0da9743dd993160e774a
kawksung <alstjd5479@naver.com>
2023-04-11 12:36:18 +0900

    DATA_JPA: @Query를 이용한 직접 query 작성하는 법 - Dto로 반환

--- inflearn/DATA_JPA/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ inflearn/DATA_JPA/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
+import study.datajpa.entity.Team;
 
 @SpringBootTest
 @Transactional
@@ -17,6 +19,8 @@ class MemberRepositoryTest {
 
   @Autowired
   MemberRepository memberRepository; //인터페이스를 인스턴스 생성? -> 프록시
+  @Autowired
+  TeamRepository teamRepository;
 
   @Test
   public void testMember() {
@@ -94,5 +98,34 @@ class MemberRepositoryTest {
 
     List<Member> result = memberRepository.findUser("AAA", 10);
     assertThat(result.get(0)).isEqualTo(m1);
+  }
+
+  @Test
+  public void findUsernameList() {
+    Member m1 = new Member("AAA", 10);
+    Member m2 = new Member("BBB", 20);
+    memberRepository.save(m1);
+    memberRepository.save(m2);
+
+    List<String> result = memberRepository.findUsernameList();
+    for (String s : result) {
+      System.out.println("s = " + s);
+    }
+  }
+
+  @Test
+  public void findMemberDto() {
+    Team team = new Team("teamA");
+    teamRepository.save(team);
+
+    Member m1 = new Member("AAA", 10);
+    m1.setTeam(team);
+    memberRepository.save(m1);
+
+
+    List<MemberDto> result = memberRepository.findMemberDto();
+    for (MemberDto memberDto : result) {
+      System.out.println("memberDto = " + memberDto);
+    }
   }
 }
