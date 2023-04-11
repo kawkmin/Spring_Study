247106c8e7c9214cc539dadb931e5bb59bd6694b
kawksung <alstjd5479@naver.com>
2023-04-11 12:19:16 +0900

    DATA_JPA: @Query를 이용한 직접 query 작성하는 법

--- inflearn/DATA_JPA/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ inflearn/DATA_JPA/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -15,4 +15,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
   //@Query(name = "Member.findByUsername") // 우선순위 1.Member에 named 쿼리랑 같은 메소드 명 2.메소드 명 쿼리
   List<Member> findByUsername(@Param("username") String username); //이름 상관 X
 
+  // 오타를 컴파일 에러로 잡아줌
+  @Query("select m from Member m where  m.username = :username and m.age = :age")
+  List<Member> findUser(@Param("username") String username, @Param("age") int age);
 }
