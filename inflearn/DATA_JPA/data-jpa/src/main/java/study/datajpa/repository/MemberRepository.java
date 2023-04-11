da4cd7b59c86d78d4f4035315d88482685a80593
kawksung <alstjd5479@naver.com>
2023-04-11 15:05:21 +0900

    DATA_JPA: 스프링 데이터 JPA 페이징과 정렬

--- inflearn/DATA_JPA/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ inflearn/DATA_JPA/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -3,6 +3,9 @@ package study.datajpa.repository;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -36,4 +39,10 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
   Member findMemberByUsername(String username);
 
   Optional<Member> findOptionalByUsername(String username);
+
+  // 3.0 버전에선 상관없나? 결과가 같음..
+  //@Query(value = "select m from Member m left join m.team t", countQuery = "select count(m) from Member m")
+  Page<Member> findByAge(int age, Pageable pageable);
+
+  //Slice<Member> findByAge(int age, Pageable pageable);
 }
--- inflearn/DATA_JPA/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ inflearn/DATA_JPA/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -8,6 +8,11 @@ import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.dto.MemberDto;
@@ -152,5 +157,32 @@ class MemberRepositoryTest {
 
     Optional<Member> findMember = memberRepository.findOptionalByUsername("asd");
     System.out.println("findMember = " + findMember);
+  }
+
+  @Test
+  public void paging() {
+    memberRepository.save(new Member(("member1"), 10));
+    memberRepository.save(new Member(("member2"), 10));
+    memberRepository.save(new Member(("member3"), 10));
+    memberRepository.save(new Member(("member4"), 10));
+    memberRepository.save(new Member(("member5"), 10));
+
+    int age = 10;
+    PageRequest pageRequest = PageRequest.of(0, 3, Sort.by(Direction.DESC, "username"));
+
+    Page<Member> page = memberRepository.findByAge(age, pageRequest);
+    //Slice<Member> page = memberRepository.findByAge(age, pageRequest); //count query 없음
+
+    Page<MemberDto> toMap = page.map(
+        member -> new MemberDto(member.getId(), member.getUsername(), null)); // api
+
+    List<Member> content = page.getContent();
+
+    assertThat(content.size()).isEqualTo(3);
+    assertThat(page.getTotalElements()).isEqualTo(5); //
+    assertThat(page.getNumber()).isEqualTo(0);
+    assertThat(page.getTotalPages()).isEqualTo(2); //
+    assertThat(page.isFirst()).isTrue();
+    assertThat(page.hasNext()).isTrue();
   }
 }
