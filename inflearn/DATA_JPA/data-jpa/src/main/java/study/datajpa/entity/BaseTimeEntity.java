68c0a29ece32f1ce4ac36432a5e6086b2d55b178
kawksung <alstjd5479@naver.com>
2023-04-11 23:37:57 +0900

    DATA_JPA: Auditing - 타임 엔티티 분리 (타임만 필요한 경우가 많기 때문)

--- inflearn/DATA_JPA/data-jpa/src/main/java/study/datajpa/entity/BaseTimeEntity.java
+++ inflearn/DATA_JPA/data-jpa/src/main/java/study/datajpa/entity/BaseTimeEntity.java
@@ -5,21 +5,19 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 import lombok.Getter;
-import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 @Getter
-public class BaseEntity extends BaseTimeEntity{
+public class BaseTimeEntity {
 
-  @CreatedBy
+  @CreatedDate
   @Column(updatable = false)
-  private String createBy;
+  private LocalDateTime createdDate;
 
-  @LastModifiedBy
-  private String lastModifiedBy;
+  @LastModifiedDate
+  private LocalDateTime lastModifiedDate;
 }
