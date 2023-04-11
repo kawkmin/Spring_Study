9b6f10a20c61f443aab518355c36f8c445d11d3f
kawksung <alstjd5479@naver.com>
2023-04-11 14:18:04 +0900

    DATA_JPA: 순수 JPA 페이징과 정렬

--- inflearn/DATA_JPA/data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ inflearn/DATA_JPA/data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -81,4 +81,24 @@ class MemberJpaRepositoryTest {
     List<Member> result = memberJpaRepository.findByUsername("AAA");
     assertThat(result.get(0)).isEqualTo(m1);
   }
+
+  @Test
+  public void paging() {
+    memberJpaRepository.save(new Member(("member1"), 10));
+    memberJpaRepository.save(new Member(("member2"), 10));
+    memberJpaRepository.save(new Member(("member3"), 10));
+    memberJpaRepository.save(new Member(("member4"), 10));
+    memberJpaRepository.save(new Member(("member5"), 10));
+
+    //page 1 offset = 0, limit = 10 / page 2 offset = 10 , limit 10
+
+    int age = 10;
+    int offset = 0;
+    int limit = 3;
+
+    List<Member> members = memberJpaRepository.findByPage(age, offset, limit);
+    Long totalCount = memberJpaRepository.totalCount(age);
+    assertThat(members.size()).isEqualTo(3);
+    assertThat(totalCount).isEqualTo(5);
+  }
 }
