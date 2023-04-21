c9b2115a3e951f81d05f0a337c9ac5736db395f9
kawksung <alstjd5479@naver.com>
2023-04-21 23:43:35 +0900

    feat: Notice Entity,Repository 생성

---
+++ personal_practice/restful/src/main/java/com/example/restful/domain/simple1/entity/Notice.java
@@ -0,0 +1,23 @@
+package com.example.restful.domain.simple1.entity;
+
+import com.example.restful.global.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Notice extends BaseEntity {
+
+  @Id
+  @GeneratedValue
+  private Long id;
+
+  private String author;
+  private String title;
+  private String content;
+}
