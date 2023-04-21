30260a7ff056294a7b5e2dcc17619043981d7747
kawksung <alstjd5479@naver.com>
2023-04-21 23:23:07 +0900

    feat: BaseEntity 생성

---
+++ personal_practice/restful/src/main/java/com/example/restful/global/entity/BaseEntity.java
@@ -0,0 +1,22 @@
+package com.example.restful.global.entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public abstract class BaseEntity {
+
+  @CreatedDate
+  private LocalDateTime registerTime;
+
+  @LastModifiedDate
+  private LocalDateTime updateTime;
+
+}
