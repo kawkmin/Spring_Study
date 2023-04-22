1c3b5c056d918e02a22fd68dad35434f31c5dc71
kawksung <alstjd5479@naver.com>
2023-04-22 14:15:39 +0900

    test: 아무튼 작성됨

---
+++ personal_practice/restful/src/test/java/com/example/restful/domain/simple1/api/NoticeControllerTest.java
@@ -0,0 +1,7 @@
+package com.example.restful.domain.simple1.api;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NoticeControllerTest {
+
+}
---
+++ personal_practice/restful/src/test/java/com/example/restful/domain/simple1/application/NoticeServiceTest.java
@@ -0,0 +1,7 @@
+package com.example.restful.domain.simple1.application;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NoticeServiceTest {
+
+}
