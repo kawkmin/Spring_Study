920471fbb32af85f4ce7995184f2af8927d6bf2b
kawksung <alstjd5479@naver.com>
2023-05-29 15:12:39 +0900

    로그인 하면 JWT 발급

--- personal_practice/restful/src/main/java/com/example/restful/domain/jwtPractice/api/UserController.java
+++ personal_practice/restful/src/main/java/com/example/restful/domain/jwtPractice/api/UserController.java
@@ -1,5 +1,7 @@
 package com.example.restful.domain.jwtPractice.api;
 
+import com.example.restful.domain.jwtPractice.application.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,10 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/users")
+@RequiredArgsConstructor
 public class UserController {
+
+  private final UserService userService;
 
   @PostMapping("/login")
   public ResponseEntity<String> login() {
-    return ResponseEntity.ok("token");
+    return ResponseEntity.ok(userService.login("", ""));
   }
 }
