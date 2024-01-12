2e938efa365417f5fc99b3d784d747e95b39780c
kawksung <alstjd5479@naver.com>
2024-01-12 09:43:22 +0900

    advanced: 예제3 - 컴포넌트 스캔으로 스프링 빈 자동 등록

--- inflearn/Advanced/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ inflearn/Advanced/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,12 +1,13 @@
 package hello.proxy;
 
+import hello.proxy.config.AppV1Config;
 import hello.proxy.config.AppV2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 //@Import(AppV1Config.class)
-@Import(AppV2Config.class)
+@Import({AppV1Config.class, AppV2Config.class})
 @SpringBootApplication(scanBasePackages = "hello.proxy.app.v3") //주의 3.0 버전엔 @Controller 필수화
 public class ProxyApplication {
 
