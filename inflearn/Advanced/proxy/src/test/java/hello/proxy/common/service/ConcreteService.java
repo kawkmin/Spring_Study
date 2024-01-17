c71fb34c00eb6fec667607a8d3de058351ba9582
kawksung <alstjd5479@naver.com>
2024-01-17 22:18:26 +0900

    advanced: CGLIB로 동적 프록시 예제 - 구체 클래스로 동적 프록시 생성 가능

---
+++ inflearn/Advanced/proxy/src/test/java/hello/proxy/common/service/ConcreteService.java
@@ -0,0 +1,14 @@
+package hello.proxy.common.service;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 인터페이스 X
+ */
+@Slf4j
+public class ConcreteService {
+
+    public void call() {
+        log.info("ConcreteService 호출");
+    }
+}
