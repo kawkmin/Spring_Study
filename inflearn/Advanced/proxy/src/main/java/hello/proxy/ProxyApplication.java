0c8c3e426d39c06d65ec122916e9f6ef6df86f8a
kawksung <alstjd5479@naver.com>
2024-01-17 21:59:22 +0900

    advanced: 동적 프록시 적용2 - 새로운 클래스를 안만들어도 됨. (filter 기능 추가)

--- inflearn/Advanced/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ inflearn/Advanced/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,6 +1,6 @@
 package hello.proxy;
 
-import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
+import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -12,7 +12,8 @@ import org.springframework.context.annotation.Import;
 //@Import({AppV1Config.class, AppV2Config.class})
 //@Import(InterfaceProxyConfig.class)
 //@Import(ConcreteProxyConfig.class)
-@Import(DynamicProxyBasicConfig.class)
+//@Import(DynamicProxyBasicConfig.class)
+@Import(DynamicProxyFilterConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app.v3") //주의 3.0 버전엔 @Controller 필수화
 public class ProxyApplication {
 
