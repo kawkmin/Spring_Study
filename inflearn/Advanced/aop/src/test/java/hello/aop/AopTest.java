44fb12a828435276d975d0d5edf47f2803555986
kawksung <alstjd5479@naver.com>
2024-01-23 21:19:24 +0900

    advanced: AOP 구현 및 적용 - 어드바이스 중복 적용

--- inflearn/Advanced/aop/src/test/java/hello/aop/AopTest.java
+++ inflearn/Advanced/aop/src/test/java/hello/aop/AopTest.java
@@ -4,7 +4,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
-import hello.aop.order.aop.AspectV2;
+import hello.aop.order.aop.AspectV3;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Import;
 
 @Slf4j
 @SpringBootTest
-@Import(AspectV2.class) // AOP 적용 (Aspect 빈 등록)
+@Import(AspectV3.class) // AOP 적용 (Aspect 빈 등록)
 public class AopTest {
 
     @Autowired
